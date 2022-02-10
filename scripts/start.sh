f806ac2920295983da2b6ec3f3d7c49f30c9046e
Turbo Xav <xavier.tagliarino@gmail.com>
2022-02-10 22:04:21 +0100

    update workflow

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 # Stop meteo-station-back
 if pm2 list | grep meteo-station-back; then pm2 delete meteo-station-back; fi
-pm2 start ./../build/meteo-station-back/main.js --name=meteo-station-back --log pm2.logs --time --max-memory-restart 200M
+pm2 start ./build/meteo-station-back/main.js --name=meteo-station-back --log pm2.logs --time --max-memory-restart 200M

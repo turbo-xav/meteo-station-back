c37386f1299600f83b3d1ff2f2a758266b1989d0
Turbo Xav <xavier.tagliarino@gmail.com>
2022-08-12 14:58:21 +0200

    Fix : removing console.log into main.ts

--- src/main.ts
+++ src/main.ts
@@ -34,8 +34,7 @@ const bootstrap = async () => {
   };*/
   // Create the Nest App Instance
   //const app = await NestFactory.create(AppModule, { httpsOptions });
-  const app = await NestFactory.create(AppModule);
-  console.log(process.env);
+  const app = await NestFactory.create(AppModule);  
   // All Url begin with "api" ex: http://urlweb.domain/api/....
   app.setGlobalPrefix('api');
 
